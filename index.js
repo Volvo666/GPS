9fd896cfb23498967075796e3aefbc98e5f8e945
Volvo666 <bosscasa1@gmail.com>
2025-06-09 20:20:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Importar middleware de autenticación
-const auth = require('./middleware/auth');
+const auth = require('./auth');
 
 // Importar rutas
 const routesRouter = require('./routes/routes');
